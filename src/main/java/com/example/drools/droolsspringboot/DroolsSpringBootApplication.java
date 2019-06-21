d318ccd97c305a7c5e5a72697eaaaee535278210
Twesh <tweshc@gmail.com>
2019-06-21 13:38:08 -0400

    initial commit

---
+++ src/main/java/com/example/drools/droolsspringboot/DroolsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.drools.droolsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsSpringBootApplication.class, args);
+	}
+
+}
